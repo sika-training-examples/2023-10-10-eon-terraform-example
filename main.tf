ca92f27f62c066e94bf0d8c4f1220268ebecf1d4
Ondrej Sika <ondrej@ondrejsika.com>
2023-10-10 10:03:57 +0200

    feat: Setup Azure provider & create resource group

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+  }
+}
+
+variable "azurerm_client_id" {}
+variable "azurerm_client_secret" {}
+variable "azurerm_tenant_id" {}
+variable "azurerm_subscription_id" {}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.azurerm_client_id
+  client_secret   = var.azurerm_client_secret
+  tenant_id       = var.azurerm_tenant_id
+  subscription_id = var.azurerm_subscription_id
+}
+
+resource "azurerm_resource_group" "eon" {
+  name     = "eon"
+  location = "westeurope"
+}
